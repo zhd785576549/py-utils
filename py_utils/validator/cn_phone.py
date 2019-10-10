55c24edc7ec0894ab6cd38bb6e2eadb4a38ea507
张冬 <zhangd@lhcis.com>
2019-10-10 13:09:19 +0800

    Add cn phone validator module

---
+++ py_utils/validator/cn_phone.py
@@ -0,0 +1,9 @@
+"""
+This is validate for phone in China
+
+Author: Winter
+History:
+    Version             Author                  Date                Message
+    1.0.0               Winter                  2019-10-10          Create
+"""
+
